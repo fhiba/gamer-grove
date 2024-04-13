0765326fa8ac3eb5d9a6bb8d23fb9af1267921d9
juanIgnacioMatilla <jmatilla@itba.edu.ar>
2024-04-13 15:29:23 -0300

    fixed setup for persistance test

--- persistance/src/test/resources/populator.sql
+++ persistance/src/test/resources/populator.sql
@@ -1,4 +1,3 @@
 INSERT INTO users(username, password, email, owner) VALUES ('Pedro', 'curti', 'pedro@curti.com', false);
 INSERT INTO community(name, description, portrait_id) VALUES ('test', 'This is a test community', null);
-INSERT INTO post_categories(category) VALUES ('Miscellaneous');
 INSERT INTO post(title, body, author_id, community_name, media, media_id, post_date,category) VALUES ('First post', 'This is my first post', 1, 'test', false, null, '2020-01-01 00:00:00','Miscellaneous');
