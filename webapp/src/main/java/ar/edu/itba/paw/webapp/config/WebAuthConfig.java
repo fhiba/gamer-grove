e124b3778119069bcee6b94a3e94e483d5625daa
juanIgnacioMatilla <jmatilla@itba.edu.ar>
2024-05-20 16:43:16 -0300

    Fixed static resources url in spring security

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -59,7 +59,7 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
     }
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**"); //Apago SpringSecurity para los assets publicos
+        web.ignoring().antMatchers("/css/**","/images/**","/js/**"); //Apago SpringSecurity para los assets publicos
     }
     @Bean
     public PasswordEncoder passwordEncoder() {
