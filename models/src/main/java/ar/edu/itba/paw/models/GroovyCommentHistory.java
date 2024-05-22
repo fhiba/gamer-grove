3f5a71ff986dafa19ded6410cf5e7d03766ad7ff
Patricio Escudeiro <pescudeiro@itba.edu.ar>
2024-05-21 22:16:27 -0300

    Updated Version of jpa

--- models/src/main/java/ar/edu/itba/paw/models/GroovyCommentHistory.java
+++ models/src/main/java/ar/edu/itba/paw/models/GroovyCommentHistory.java
@@ -1,33 +1,127 @@
 package ar.edu.itba.paw.models;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "groovy_comment_history")
+@IdClass(GroovyCommentHistory.GroovyCommentHistoryId.class)
 public class GroovyCommentHistory {
 
-    private final int userId;
-    private final int commentId;
-    private final int postId;
-    private final boolean grooviness;
+    @Id
+    @JoinColumn(name = "user_id")
+    @OneToOne(targetEntity = User.class)
+    private User user;
 
-    public GroovyCommentHistory(int userId, int commentId, int postId, boolean grooviness) {
-        this.userId = userId;
-        this.commentId = commentId;
-        this.postId = postId;
+    @Id
+    @JoinColumn(name = "comment_id")
+    @OneToOne(targetEntity = Comment.class)
+    private Comment comment;
+
+    @Id
+    @JoinColumn(name = "post_id")
+    @OneToOne(targetEntity = Post.class)
+    private Post post;
+    @Column(name="groovy_type")
+    private  boolean grooviness;
+
+    public GroovyCommentHistory(User user, Comment comment, Post post, boolean grooviness) {
+        this.user = user;
+        this.comment = comment;
+        this.post = post;
         this.grooviness = grooviness;
     }
 
+    public GroovyCommentHistory() {
 
-    public int getUserId() {
-        return userId;
     }
 
-    public int getCommentId() {
-        return commentId;
+
+    public User getUser() {
+        return user;
+    }
+
+    public Comment getComment() {
+        return comment;
     }
 
     public boolean isGrooviness() {
         return grooviness;
     }
 
-    public int getPostId() {
-        return postId;
+    public Post getPost() {
+        return post;
+    }
+
+    public void setUserId(User user) {
+        this.user = user;
+    }
+
+    public void setCommentId(Comment comment) {
+        this.comment = comment;
+    }
+
+    public void setPostId(Post post) {
+        this.post = post;
+    }
+
+    public void setGrooviness(boolean grooviness) {
+        this.grooviness = grooviness;
+    }
+
+    public static class GroovyCommentHistoryId implements Serializable {
+        private User user;
+
+        private Comment comment;
+
+        private Post post;
+
+        public GroovyCommentHistoryId(User user, Comment comment, Post post) {
+            this.user = user;
+            this.comment = comment;
+            this.post = post;
+        }
+
+        public GroovyCommentHistoryId() {
+        }
+
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GroovyCommentHistoryId that = (GroovyCommentHistoryId) o;
+            return Objects.equals(user, that.user) && Objects.equals(comment, that.comment) && Objects.equals(post, that.post);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(user, comment, post);
+        }
+
+        public User getUser() {
+            return user;
+        }
+
+        public void setUser(User user) {
+            this.user = user;
+        }
+
+        public Comment getComment() {
+            return comment;
+        }
+
+        public void setComment(Comment comment) {
+            this.comment = comment;
+        }
+
+        public Post getPost() {
+            return post;
+        }
+
+        public void setPost(Post post) {
+            this.post = post;
+        }
     }
 }
--- models/src/main/java/ar/edu/itba/paw/models/Post.java
+++ models/src/main/java/ar/edu/itba/paw/models/Post.java
@@ -12,40 +12,45 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "post_id_seq")
     @SequenceGenerator(sequenceName = "post_id_seq", name = "post_id_seq", allocationSize = 1)
     @Column(name ="id")
-    private final long id;
+    private  long id;
     @Column(name = "title", nullable = false)
-    private final String title;
+    private  String title;
     @Column(name = "body", nullable = false)
-    private final String body;
+    private  String body;
 
     @ManyToOne(targetEntity = User.class)
     @JoinColumn(name = "author_id", referencedColumnName = "id")
-    private final User authorId;
+    private User author;
 
     @ManyToOne(targetEntity = Community.class)
     @JoinColumn(name = "community_name", referencedColumnName = "name")
-    private final String communityName;
+    private Community community;
 
     @Column(name = "media", nullable = false)
-    private final boolean media;
+    private  boolean media;
 
-    private final long mediaId;
+    @Column(name = "media_id")
+    private  Long mediaId;
 
     @Column(name= "post_date", nullable = false)
-    private final LocalDateTime date;
+    private  LocalDateTime date;
     @Column (name = "grooviness", nullable = false)
-    private final int grooviness;
+    private  int grooviness;
     @Column(name = "deleted", nullable = false)
-    private final boolean deleted;
+    private  boolean deleted;
 
-
-    private final String category;
+    @Column(name = "category")
+    private String category;
 
     @OneToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "post_images", joinColumns = {@JoinColumn(name = "post_id")}, inverseJoinColumns = {@JoinColumn(name = "image_id")})
     private List<File> images;
 
-    
+
+    public Post(){
+        //HIBERNATE ONLY
+    }
+
     public long getId() {
         return id;
     }
@@ -58,12 +63,12 @@ public class Post {
         return body;
     }
 
-    public long getAuthorId() {
-        return authorId;
+    public User getAuthor() {
+        return author;
     }
 
-    public String getCommunityName() {
-        return communityName;
+    public Community getcommunity() {
+        return community;
     }
 
     public boolean getMedia() {
@@ -84,12 +89,12 @@ public class Post {
     public String getCategory() {
         return category;
     }
-    public Post(final long id, final String title, final String body, final long author_id, final String community_name, final boolean media, final long media_id, final LocalDateTime date, final int grooviness, boolean deleted, final String category) {
+    public Post(final long id, final String title, final String body, final User author, final Community community, final boolean media, final long media_id, final LocalDateTime date, final int grooviness, boolean deleted, final String category) {
         this.id = id;
         this.title = title;
         this.body = body;
-        this.authorId = author_id;
-        this.communityName = community_name;
+        this.author = author;
+        this.community = community;
         this.media = media;
         this.mediaId = media_id;
         this.date = date;
@@ -103,15 +108,15 @@ public class Post {
     }
 
 
-    public List<Integer> getImages() {
+    public List<File> getImages() {
         return images;
     }
 
-    public void setImages(List<Integer> images) {
+    public void setImages(List<File> images) {
         this.images = images;
     }
 
-    public String getEncodedCommunityName(){
-        return URLEncoder.encode(communityName, StandardCharsets.UTF_8);
+    public String getEncodedcommunity(){
+        return URLEncoder.encode(community.getName(), StandardCharsets.UTF_8);
     }
 }
