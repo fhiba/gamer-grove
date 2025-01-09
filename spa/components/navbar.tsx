16b6fb38817a0998084224f65b7b1cf3d1f7529a
kerty dev <pkerty.dev@gmail.com>
2025-01-09 13:40:23 -0300

    navbar in frontend

--- spa/components/navbar.tsx
+++ spa/components/navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Navbar as NextUINavbar,
   NavbarContent,
@@ -7,24 +9,27 @@ import {
   NavbarItem,
   NavbarMenuItem,
 } from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
-import { Link } from "@nextui-org/link";
+import { Button, ButtonGroup } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
+import { Link } from "@nextui-org/link";
+import { Avatar, AvatarGroup, AvatarIcon } from "@nextui-org/avatar";
+
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  DropdownSection,
+} from "@nextui-org/dropdown";
+
+import { User } from "@nextui-org/user";
+
 import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
-import clsx from "clsx";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
-import {
-  TwitterIcon,
-  GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
-  Logo,
-} from "@/components/icons";
+import { SearchIcon, Logo } from "@/components/icons";
 
 export const Navbar = () => {
   const searchInput = (
@@ -34,13 +39,8 @@ export const Navbar = () => {
         inputWrapper: "bg-default-100",
         input: "text-sm",
       }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
       labelPlacement="outside"
-      placeholder="Search..."
+      placeholder="Search  for communities..."
       startContent={
         <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
       }
@@ -48,21 +48,57 @@ export const Navbar = () => {
     />
   );
 
+  // TODO: proper href
+  const SignInButton = ({ isLogged = false }: { isLogged?: boolean }) => (
+    <div>
+      {!isLogged ? (
+        <Button color="primary" size="md" variant="shadow" as={Link} href="/#">
+          Sign In
+        </Button>
+      ) : (
+        <Dropdown placement="bottom-start">
+          <DropdownTrigger>
+            <User
+              as="button"
+              avatarProps={{
+                isBordered: true,
+                src: "https://i.pravatar.cc/150?u=a042581f4e29026024d",
+              }}
+              className="transition-transform"
+              name="Tony Reichert"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="User Actions" variant="flat">
+            <DropdownSection>
+              <DropdownItem key="profile">My Profile</DropdownItem>
+
+              <DropdownItem key="logout" color="danger">
+                Log Out
+              </DropdownItem>
+            </DropdownSection>
+          </DropdownMenu>
+        </Dropdown>
+      )}
+    </div>
+  );
+
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
-      <NavbarContent className="basis-1/6" justify="start">
+      <NavbarContent className="basis-2/6" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-xl">GamerGrove</p>
           </NextLink>
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="basis-4/6" justify="center">
+      <NavbarContent className="basis-3/6" justify="center">
         <NavbarItem className="w-full">{searchInput}</NavbarItem>
       </NavbarContent>
-      <NavbarContent className="basis-1/6">Falopa</NavbarContent>
+      <NavbarContent className="basis-1/6" justify="end">
+        <SignInButton isLogged={true} />
+      </NavbarContent>
     </NextUINavbar>
   );
 };
